b676ee500ca5c36e12c73c7e60df17082ee76729
Facundo <facuquinterosrc@hotmail.com>
2025-03-27 23:00:08 -0300

    Implementacion de BFS de grafo

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -16,12 +16,49 @@ class BreadthFirstSearch:
             Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        node = Node("", grid.start,0,None,None)
+
 
         # Initialize the explored dictionary to be empty
         explored = {} 
         
+        if node.state == grid.end:
+            return Solution(node, explored)
+
+        # Initialize the frontier with the initial node
+        # In this example, the frontier is a queue
+        frontier = QueueFrontier()
+        frontier.add(node)
+
         # Add the node to the explored dictionary
         explored[node.state] = True
         
-        return NoSolution(explored)
+        while True:
+
+            print("Primera parte")
+
+            if frontier.is_empty():
+                print("Frontera vacia")
+                return NoSolution(explored)
+
+            #Remover nodo de la frontera
+            node = frontier.remove()
+
+            successors = grid.get_neighbours(node.state)
+            
+
+            for action,state in successors.items():
+                print("recorrido de actions")
+                
+                if state not in explored:
+                    new_node =  Node("", state, node.cost + grid.get_cost(state), parent=node, action=action)
+
+                    if new_node.state == grid.end:
+                        # running = False
+                        return Solution(new_node, explored)
+
+                    explored[state] = True
+                    frontier.add(new_node)
+
+                    
+
