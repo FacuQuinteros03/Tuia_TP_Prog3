b49d1e7b9290f43a76b6cc970d257ff859cb5e4c
Genaro <genarocanciani@gmail.com>
2025-04-17 16:01:00 -0300

    Implementación algoritmos GBFS y A*

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -3,6 +3,11 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
+def h(state:tuple[int,int],target_state:tuple[int,int]) -> int: 
+    "Return heuristic value from a node to a target state." 
+    x1,y1 = state
+    x2,y2 = target_state
+    return abs(x1 - x2) + abs(y1 - y2)
 
 class GreedyBestFirstSearch:
     @staticmethod
@@ -15,13 +20,41 @@ class GreedyBestFirstSearch:
         Returns:
             Solution: Solution found
         """
+        
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        node = Node("", grid.start,0,None,0)
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
-        
+        frontier = PriorityQueueFrontier()
+
+        frontier.add(node,h(node.state,grid.end))
+
         # Add the node to the explored dictionary
-        explored[node.state] = True
+        explored = {node.state: node.cost}
         
-        return NoSolution(explored)
+        while True:
+
+            print("Primera parte")
+
+            if frontier.is_empty():
+                print("Frontera vacia")
+                return NoSolution(explored)
+
+            #Remover nodo de la frontera
+            node = frontier.pop()
+            
+            if node.state == grid.end:
+                return Solution(node, explored)
+
+            successors = grid.get_neighbours(node.state)
+            
+            for action,state in successors.items():
+                print("recorrido de actions")
+                new_cost = node.cost + grid.get_cost(state) 
+                
+
+                if state not in explored or new_cost < explored[new_node.state]:
+                    new_node =  Node("", state, new_cost, parent=node, action=action)
+                    explored[state] = new_cost
+                    frontier.add(new_node,h(new_node.state,grid.end))
+
+
